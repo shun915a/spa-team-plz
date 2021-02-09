d3e81c5632c55b41ec541182b8047929874f8c38
shun915a <74646604+shun915a@users.noreply.github.com>
2021-02-09 18:34:22 +0900

    update user and friend model

---
+++ app/models/friend.rb
@@ -0,0 +1,11 @@
+class Friend < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :title
+    validates :game_id
+    validates :text
+  end
+
+  scope :active, -> { where(active: true) }
+end
