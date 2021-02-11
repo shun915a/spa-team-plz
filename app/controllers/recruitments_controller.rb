137ab2abcbb9c6e93e73c2292b6946099fee34f2
shun915a <74646604+shun915a@users.noreply.github.com>
2021-02-11 10:43:48 +0900

    react

---
+++ app/controllers/recruitments_controller.rb
@@ -0,0 +1,7 @@
+class RecruitmentsController < ApplicationController
+  def index
+    render json: {
+
+    }, status: :ok
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      devise_for :users
-      resources :recruitments, only: %i[index]
-      resources :friends
-    end
-  end
+  devise_for :users
+  resources :recruitments, only: %i[index]
+  resources :friends
 end
